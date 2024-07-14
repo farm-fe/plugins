600118fbd5ad74b8f07c09cd2ca1da18d1013da9
maidang1 <497350746@qq.com>
2024-07-14 21:58:19 +0800

    feat: update code

--- packages/vue-markdown/src/lib.rs
+++ packages/vue-markdown/src/lib.rs
@@ -1,11 +1,18 @@
 #![deny(clippy::all)]
 
-mod utils;
 mod options;
 mod plugin_component;
-use farmfe_core::{config::Config, plugin::Plugin};
+mod utils;
+mod core;
+
+use farmfe_core::{
+  config::Config,
+  module::ModuleType,
+  plugin::{Plugin, PluginLoadHookResult},
+};
 
 use farmfe_macro_plugin::farm_plugin;
+use farmfe_toolkit::fs::read_file_utf8;
 
 #[farm_plugin]
 pub struct VueMarkdown {}
@@ -19,5 +26,33 @@ impl VueMarkdown {
 impl Plugin for VueMarkdown {
   fn name(&self) -> &str {
     "VueMarkdown"
+  }
+
+  fn load(
+    &self,
+    param: &farmfe_core::plugin::PluginLoadHookParam,
+    _context: &std::sync::Arc<farmfe_core::context::CompilationContext>,
+    _hook_context: &farmfe_core::plugin::PluginHookContext,
+  ) -> farmfe_core::error::Result<Option<farmfe_core::plugin::PluginLoadHookResult>> {
+    if param.resolved_path.ends_with((".mdx")) || param.resolved_path.ends_with(".md") {
+      let content = read_file_utf8(param.resolved_path).unwrap();
+
+      return Ok(Some(PluginLoadHookResult {
+        content,
+        source_map: None,
+        module_type: ModuleType::Custom("vue-markdown".to_string()),
+      }));
+    }
+    Ok(None)
+  }
+
+  fn transform(
+    &self,
+    param: &farmfe_core::plugin::PluginTransformHookParam,
+    _context: &std::sync::Arc<farmfe_core::context::CompilationContext>,
+  ) -> farmfe_core::error::Result<Option<farmfe_core::plugin::PluginTransformHookResult>> {
+    if param.module_type != ModuleType::Custom("vue-markdown".to_string()) {
+      return Ok(None);
+    }
   }
 }
