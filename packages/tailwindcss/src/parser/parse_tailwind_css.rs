e131007bc3886ff79b3c0056e268385654579096
CCherry07 <2405693142@qq.com>
2024-09-02 09:42:01 +0800

    feat: parse_tailwind_css_with_changed

--- packages/tailwindcss/src/parser/parse_tailwind_css.rs
+++ packages/tailwindcss/src/parser/parse_tailwind_css.rs
@@ -1,7 +1,10 @@
 use std::path::PathBuf;
 use tailwind_ast::parse_tailwind;
 use tailwind_css::{TailwindBuilder, TailwindInstruction};
-use tailwindcss_oxide::{scanner::detect_sources::DetectSources, GlobEntry, Scanner};
+use tailwindcss_oxide::{
+  scanner::detect_sources::DetectSources, ChangedContent, GlobEntry, Scanner,
+};
+
 pub fn filter_tailwind_atom_css(css: Vec<String>) -> Vec<String> {
   let need_filter = vec![
     "content".to_string(),
@@ -25,7 +28,7 @@ pub fn parse_tailwind_css(
   tw_builder: &mut TailwindBuilder,
   base: &str,
   contents: Vec<String>,
-) -> String {
+) -> (String, Scanner) {
   let sources = contents
     .iter()
     .map(|c| {
@@ -37,6 +40,27 @@ pub fn parse_tailwind_css(
     .collect();
   let mut scanner = Scanner::new(Some(DetectSources::new(PathBuf::from(base))), Some(sources));
   let res = scanner.scan();
+  let styles: String = filter_tailwind_atom_css(res).join(" ");
+  tw_builder.trace(&styles, false).unwrap();
+  let bundle = tw_builder.bundle().unwrap();
+  (bundle, scanner)
+}
+
+pub fn parse_tailwind_css_with_changed(
+  tw_builder: &mut TailwindBuilder,
+  scanner: &mut Scanner,
+  changed_files: Vec<String>,
+) -> String {
+  let changed_content = changed_files
+    .iter()
+    .map(|c| {
+      return ChangedContent {
+        file: Some(PathBuf::from(c)),
+        content: None,
+      };
+    })
+    .collect();
+  let res = scanner.scan_content(changed_content);
   let styles: String = filter_tailwind_atom_css(res).join(" ");
   tw_builder.trace(&styles, false).unwrap();
   let bundle = tw_builder.bundle().unwrap();
