35c763d86079e67427488b6b992d6fce49aa2965
CCherry07 <2405693142@qq.com>
2024-09-09 00:14:37 +0800

    fix(auto-import): options type

--- packages/auto-import/options.d.ts
+++ packages/auto-import/options.d.ts
@@ -1,13 +1,49 @@
-
-type TPreset = string | { from: string, imports: string[] } | {
-  [key: string]: string | string[]
-}
+export type ImportNameAlias = [string, string]
+export type InlinePreset = { from: string, imports: string[] }
+type Preset = string
+  | InlinePreset
+  | Record<string, (string | ImportNameAlias)[]>
 
 export interface IPluginOptions {
-  dirs: string[];
-  dts: boolean | string;
-  ignore: string[];
-  include: string[];
-  exclude: string[];
-  presets: TPreset[];
+  /**
+   * The directories to search for apis.
+   * @default process.cwd()
+   * @example
+   * dirs: ["src/addons"]
+   */
+  dirs?: string[];
+  /**
+   * generate d.ts file.
+   * @default true
+   * @example
+   * dts: "src/types/auto-import.d.ts"
+   */
+  dts?: boolean | string;
+  /**
+   * The ignore patterns.
+   * @example
+   * ignore: ["useFoo","^my*"]
+   */
+  ignore?: string[];
+  /**
+   * The presets of the project.
+   * @example
+   * presets: [
+   *  "react",
+   *  {
+   *    from: "react",
+   *    imports: ["useState","useEffect"]
+   *  }, 
+   *  {
+   *    "react-router-dom": 
+   *      [
+   *        "useHistory",
+   *        ["useParams","useReactPrams"]
+   *     ]
+   *  }
+   * ]
+   */
+  presets?: Preset[];
+  include?: string[];
+  exclude?: string[];
 }
