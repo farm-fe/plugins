77a1172cfbfe194a9d5018531ba5ebca48c45227
Madinah <497350746@qq.com>
2024-09-25 20:13:25 +0800

    feat(all):  增加 js-plugins (#74)
    * feat(none): 增加 commit 校验
    * feat(all): 🎸 增加 js-plugins

---
+++ commitlint.config.js
@@ -0,0 +1,41 @@
+const pc = require("picocolors");
+
+const scoreRule = (parsed) => {
+  const validScores = ["js-plugins", "rust-plugins", "all", "none"];
+  const isValid = validScores.includes(parsed.scope);
+
+  return [
+    isValid,
+    `current scope is ${pc.red(parsed.scope)}, ${pc.green(
+      "because we need the right scope to do CI dispatch"
+    )} .Mark ${pc.yellowBright(
+      "js-plugins"
+    )} if you need to change the ${pc.yellowBright("js package")}.
+    Mark ${pc.yellowBright(
+      "rust-plugins"
+    )} if you need to change the ${pc.yellowBright(
+      "rust package"
+    )} mark ${pc.yellowBright("all")} if you have ${pc.yellowBright(
+      "both"
+    )} and ${pc.yellowBright("none if you don't need to send packages")}`,
+  ];
+};
+
+module.exports = {
+  rules: {
+    // "scope-enum": [2, "always", ["js-plugins", "rust-plugins", "all", "none"]],
+    "score-rule": [2, "always"],
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "perf", "test", "chore"],
+    ],
+  },
+  plugins: [
+    {
+      rules: {
+        "score-rule": scoreRule,
+      },
+    },
+  ],
+};
