7135af266932faba38497bda2f7894c77c0ad6b4
xiaoliyo <2585960098@qq.com>
2025-04-29 21:28:18 +0800

    feat: playground add comlink worker

--- rust-plugins/worker/playground/src/index.tsx
+++ rust-plugins/worker/playground/src/index.tsx
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import { Main } from './main';
 import TestWorker from "./worker/test.worker?worker"
+import ComlinkWorker from "./worker/comlink.worker?worker"
 import './index.css'
+import { MyWorker } from './worker/comlink.worker.ts';
 
 // console.log(TestWorker);
 // const worker = new TestWorker();
@@ -12,10 +14,16 @@ worker.onmessage = (e) => {
 }
 const worker2 = new Worker(new URL("./worker/vue.worker.ts",import.meta.url))
 
+
 worker2.postMessage([2, 3]);
 worker2.onmessage = (e) => {
   console.log(e.data);
 }
+
+const comlinkWorker = (new ComlinkWorker() as unknown as MyWorker );
+comlinkWorker.add(1, 2).then((res) => {
+  console.log(res); // 3
+})
 
 const container = document.querySelector('#root');
 const root = createRoot(container!);
